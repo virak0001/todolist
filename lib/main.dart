8cc6bdb5a58126d3f3716d76bda63025266816fa
Virak Ran <virakcambodia44@gmail.com>
2020-01-23 11:38:18 +0700

    last change

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'modalDialog.dart';
+import 'displayList.dart';
 
 void main() => runApp(Home());
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ModalDialog(),
+      home: DiesplayList(),
     );
   }
 }
